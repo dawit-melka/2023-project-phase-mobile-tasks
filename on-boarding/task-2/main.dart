1dcb6b6bb3318dc3aef805a57ca98fbf4dfe1de0
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-08-01 19:07:45 +0300

    Update main.dart

--- on-boarding/task-2/main.dart
+++ on-boarding/task-2/main.dart
@@ -79,25 +79,3 @@ void main(){
   }
 }
 
-class Task {
-  String Title, Description;
-  DateTime Duedate;
-  bool Status;
-
-  Task(this.Title, this.Description, this.Duedate, this.Status);
-
-  void display(){
-    print("Title: ${this.Title}");
-    print("Description: ${this.Description}");
-    print("Duedate: ${this.Duedate}");
-    if(this.Status){
-      print("Status: completed");
-    }else{
-      print("Status: pending");
-    }
-  }
-
-  void completed(){
-    this.Status = true;
-  }
-}
